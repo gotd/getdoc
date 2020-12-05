e29203a0ebe742dcf883bae4f5c0b527e193655b
Aleksandr Razumov <ar@gortc.io>
2020-12-05 20:59:07 +0300

    feat(getdoc): implement command

--- cmd/getdoc/main.go
+++ cmd/getdoc/main.go
@@ -1,7 +1,60 @@
+// Binary getdoc extracts Telegram documentation to json file.
 package main
 
-import "fmt"
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+
+	"github.com/gotd/getdoc"
+	"github.com/gotd/getdoc/dl"
+)
 
 func main() {
-	fmt.Println("hello")
+	dir := flag.String("dir", filepath.Join(os.TempDir(), "getdoc"), "working directory")
+	readonly := flag.Bool("readonly", false, "read-only mode")
+	pretty := flag.Bool("pretty", false, "pretty json output")
+	flag.Parse()
+
+	client, err := dl.NewClient(dl.Options{
+		Path:     filepath.Join(*dir, "cache"),
+		Readonly: *readonly,
+	})
+	if err != nil {
+		panic(err)
+	}
+	defer func() {
+		if err := client.Close(); err != nil {
+			panic(err)
+		}
+	}()
+
+	ctx := context.Background()
+	fmt.Println("Extracting")
+	doc, err := getdoc.Extract(ctx, client)
+	if err != nil {
+		panic(err)
+	}
+
+	out := new(bytes.Buffer)
+	enc := json.NewEncoder(out)
+	if *pretty {
+		enc.SetIndent("", "  ")
+	}
+	if err := enc.Encode(doc); err != nil {
+		panic(err)
+	}
+
+	outFileName := fmt.Sprintf("%d.json", doc.Index.Layer)
+	outFilePath := filepath.Join(*dir, outFileName)
+	if err := ioutil.WriteFile(outFilePath, out.Bytes(), 0600); err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Wrote layer", doc.Index.Layer, "to", outFilePath)
 }
